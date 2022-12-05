73cfd90442382879de87815b4f02cb481caabc3d
刘贤金 <liuxianjin@dfsoft.com.cn>
2022-12-05 15:22:58 +0800

    目录文件整理

--- codeSharing/operaor/new.js
+++
@@ -1,6 +0,0 @@
-CommonComp.getSurgeryRoomCombo(me.win.userInfo, {
-  width: 180,
-  hidden: true,
-  fieldLabel: me.win.prefix === "anes" ||  me.win.prefix === "anesDoctor" ? "麻醉科" : "手术室",
-  extraParamsType: me.win.prefix === "anes" || me.win.prefix === "anesDoctor" ? "anes" : null //anes代表麻醉数据
-})
